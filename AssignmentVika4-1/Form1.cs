7d0de983a2725a824494bdad1b4d7e8d2a9188b9
vika-anikina <tiltoslave@gmail.com>
2024-10-16 16:44:39 +0300

    first commit

---
+++ AssignmentVika4-1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentVika4_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private long CalculateFactorial(int n)
+        {
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int number) && number >= 0)
+            {
+                long factorial = CalculateFactorial(number);
+                label2.Text = $"Factorial of {number} is {factorial}";
+            }
+            else
+            {
+                MessageBox.Show("Please enter nonnegative integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
